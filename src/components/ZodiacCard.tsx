6c81b770cddd9125ac5981c918c4243856309a75
thisdotEric <siguenzajohneric@gmail.com>
2021-05-02 21:38:42 +0800

    Applied effects when zodiac card is clicked

--- src/components/ZodiacCard.tsx
+++ src/components/ZodiacCard.tsx
@@ -1,59 +1,87 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import "./ZodiacCard.css";
+import { zodiacSigns } from "../data/zodiacDates";
 
 interface ZodiacCardProps {
-  zodiacName: string;
-  startDate: string;
-  endDate: string;
   setZodiac: (zodiac: string) => void;
-  effects?: IClickedZodiacEffects;
-  setRef: (el: HTMLDivElement) => void;
-}
-
-export interface IClickedZodiacEffects {
-  direction: string;
-  isHighlight: boolean;
-  isHiddenDate: boolean;
 }
 
 // Renders zodiac cards
-const ZodiacCard: React.FC<ZodiacCardProps> = ({
-  zodiacName,
-  startDate,
-  endDate,
-  setZodiac,
-  effects,
-  setRef,
-}) => {
-  const [addSlideEffect, setaddSlideEffect] = useState<string>("");
-  const [highlight, sethighlight] = useState<string>("card");
-  const [hideDate, sethideDate] = useState<string>("date");
+const ZodiacCard: React.FC<ZodiacCardProps> = ({ setZodiac }) => {
+  const zodiacRef = useRef<HTMLDivElement[]>([]);
+  zodiacRef.current = [];
+
+  const addDivElement = (divElement: HTMLDivElement) => {
+    if (divElement && !zodiacRef.current.includes(divElement)) {
+      zodiacRef.current.push(divElement);
+    }
+  };
+
+  const zodiacClick = (zodiacName: string, zodiacNode: HTMLDivElement) => {
+    // Set choosen zodiac
+    setZodiac(zodiacName);
+
+    // Add a slide down effect
+    zodiacNode.classList.add("slide");
+    // Add highlight effect
+    zodiacNode.querySelector("#img")?.classList.remove("card");
+    zodiacNode.querySelector("#img")?.classList.add("highlight");
+    // Remove bottom date
+    zodiacNode.querySelector("#date")?.classList.remove("date");
+    zodiacNode.querySelector("#date")?.classList.add("hide");
+  };
+
+  const animateUp = (zodiacNode: HTMLDivElement) => {
+    zodiacNode.classList.remove("slide");
+
+    zodiacNode.classList.add("slide-up");
+
+    // Remove highlight effect
+    zodiacNode.querySelector("#img")?.classList.remove("highlight");
+    zodiacNode.querySelector("#img")?.classList.add("card");
+
+    // Show bottom date
+    zodiacNode.querySelector("#date")?.classList.add("date");
+    zodiacNode.querySelector("#date")?.classList.remove("hide");
+
+    // Remove unnecessary slide-up effect class
+    zodiacNode.classList.remove("slide-up");
+  };
 
   return (
-    <div
-      ref={setRef}
-      className={`zodiac ${addSlideEffect ? "slide" : ""}`}
-      onClick={() => {
-        if (effects !== null) {
-          setaddSlideEffect(effects!.direction);
-          sethighlight(effects!.isHighlight ? "highlight" : "");
-          sethideDate(effects!.isHiddenDate ? "hide" : "");
-          setZodiac(zodiacName);
-        }
-      }}
-    >
-      <div className={highlight}>
-        <img
-          src={require(`../assets/zodiacSvgs/${zodiacName}.png`).default}
-          className="zodiacLogo"
-          alt="svg"
-        />
-        <p className="zodiacName">{zodiacName}</p>
-      </div>
-      <div className={hideDate}>
-        <p>{startDate}</p>
-        <p>{endDate}</p>
-      </div>
+    <div className="cards">
+      {zodiacSigns.map(zodiac => (
+        <div
+          ref={addDivElement}
+          key={zodiac.name}
+          className={`zodiac`}
+          onClick={() => {
+            zodiacRef.current.forEach(zodiacDiv => {
+              if (zodiacDiv.classList.contains("slide")) animateUp(zodiacDiv);
+
+              if (
+                zodiacDiv.querySelector(".card p")?.innerHTML === zodiac.name
+              ) {
+                zodiacClick(zodiac.name, zodiacDiv);
+                return;
+              }
+            });
+          }}
+        >
+          <div className="card" id="img">
+            <img
+              src={require(`../assets/zodiacSvgs/${zodiac.name}.png`).default}
+              className="zodiacLogo"
+              alt="svg"
+            />
+            <p className="zodiacName">{zodiac.name}</p>
+          </div>
+          <div className="date" id="date">
+            <p>{zodiac.dateStart}</p>
+            <p>{zodiac.dateEnd}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
